2d6e93c9ecb46916bc3052c93052c95a238bdbc3
Collin Butterworth <cb264195@falmouth.ac.uk>
2023-03-10 15:30:27 +0000

    Wrote a function for moving to the nearest sink

--- Unity/COMP250 AI Project/Assets/NPC_Controller.cs
+++ Unity/COMP250 AI Project/Assets/NPC_Controller.cs
@@ -12,6 +12,14 @@ public class NPC_Controller : MonoBehaviour
     public int energy;
     public int boredom;
 
+    public string target;
+    public float speed;
+    float distanceToNearestStation;
+    float distance;
+    GameObject[] targetObjects;
+    GameObject closestTarget = null;
+
+
     void Start ()
     {
         this.enabled = true;
@@ -19,6 +27,24 @@ public class NPC_Controller : MonoBehaviour
 
     void Drink()
     {
+        distanceToNearestStation = Mathf.Infinity;
+        target = "sink";
+        GameObject[] targetObjects = GameObject.FindGameObjectsWithTag(target);
 
+        foreach (GameObject targetObject in targetObjects)
+        {
+            distance = Vector3.Distance(transform.position, targetObject.transform.position);
+
+            if (distance < distanceToNearestStation) 
+            {
+                distanceToNearestStation = distance;
+                closestTarget = targetObject;
+            }
+        }
+
+        if (closestTarget != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, closestTarget.transform.position, speed * Time.deltaTime);
+        }
     }
 }
