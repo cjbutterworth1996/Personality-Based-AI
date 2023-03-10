d288e44fdb90afe1ecc548eee4758fda22ac6e04
Collin Butterworth <cb264195@falmouth.ac.uk>
2023-03-10 19:25:44 +0000

    Added fridge prefab and spawn

--- Unity/COMP250 AI Project/Assets/StationController.cs
+++ Unity/COMP250 AI Project/Assets/StationController.cs
@@ -4,30 +4,37 @@ using UnityEngine;
 
 public class StationController : MonoBehaviour
 {
-    public GameObject sinkPrefab;
-    public GameObject toiletPrefab;
-    public GameObject bedPrefab;
-    public int numberOfSinks;
-    public int numberOfToilets;
-    public int numberOfBeds;
-    public Transform[] sinkArray;
-    public Transform[] toiletArray;
-    public Transform[] bedArray;
+	public GameObject bedPrefab;
+	public GameObject fridgePrefab;
+	public GameObject sinkPrefab;
+	public GameObject toiletPrefab;
+	public int numberOfBeds;
+	public int numberOfFridges;
+	public int numberOfSinks;
+	public int numberOfToilets;
+	public Transform[] bedArray;
+	public Transform[] fridgeArray;
+	public Transform[] sinkArray;
+	public Transform[] toiletArray;
 
-    // Start is called before the first frame update
-    private void Start()
-    {
-        for (int i = 0; i < numberOfSinks; i++)
-        {
-            Instantiate(sinkPrefab, sinkArray[i].position, sinkArray[i].rotation);
-        }
-        for (int i = 0; i < numberOfToilets; i++)
-        {
-            Instantiate(toiletPrefab, toiletArray[i].position, toiletArray[i].rotation);
-        }
+	// Start is called before the first frame update
+	private void Start()
+	{
         for (int i = 0; i < numberOfBeds; i++)
         {
             Instantiate(bedPrefab, bedArray[i].position, bedArray[i].rotation);
         }
-    }
+        for (int i = 0; i < numberOfFridges; i++)
+        {
+            Instantiate(fridgePrefab, fridgeArray[i].position, fridgeArray[i].rotation);
+        }
+        for (int i = 0; i < numberOfSinks; i++)
+		{
+			Instantiate(sinkPrefab, sinkArray[i].position, sinkArray[i].rotation);
+		}
+		for (int i = 0; i < numberOfToilets; i++)
+		{
+			Instantiate(toiletPrefab, toiletArray[i].position, toiletArray[i].rotation);
+		}
+	}
 }
