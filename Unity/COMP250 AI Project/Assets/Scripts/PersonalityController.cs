79a5a18eef344d62951680564841268a668bdace
Collin Butterworth <cb264195@falmouth.ac.uk>
2023-03-18 17:06:15 +0000

    Added a script for generating personalities

--- Unity/COMP250 AI Project/Assets/Scripts/PersonalityController.cs
+++ Unity/COMP250 AI Project/Assets/Scripts/PersonalityController.cs
@@ -18,6 +18,9 @@ public class PersonalityController : MonoBehaviour
     public float minMoneyThreshold;
     public float maxMoneyThreshold;
     public float randomizationFactor;
+    public float minWeight;
+    public float maxWeight;
+    public float totalWeight;
     public int moneyNeededPerDay;
     public NPCController npcController;
     public bool needsMoney;
@@ -33,9 +36,26 @@ public class PersonalityController : MonoBehaviour
         maxMoneyThreshold = maxMoney;
     }
 
-    void GeneratePersonality (string optionalTag)
+    void GeneratePersonality ()
     {
+        // This sets the min and max weight for each need.
+        minWeight = 0.1f;
+        maxWeight = 1f;
 
+        // This generates a random weight for each need within the set parameters.
+        bladderWeight = Random.Range(minWeight, maxWeight);
+        boredomWeight = Random.Range(minWeight, maxWeight);
+        energyWeight = Random.Range(minWeight, maxWeight);
+        hungerWeight = Random.Range(minWeight, maxWeight);
+        thirstWeight = Random.Range(minWeight, maxWeight);
+
+        // Normalizes all of the weights to add up to 1. This is to ensure all of the probabilities add up to 100%.
+        totalWeight = bladderWeight+ boredomWeight + energyWeight + hungerWeight + thirstWeight;
+        bladderWeight /= totalWeight;
+        boredomWeight /= totalWeight;
+        energyWeight /= totalWeight;
+        hungerWeight /= totalWeight;
+        thirstWeight /= totalWeight;
     }
 
     // Creates a reward for fulfilling a need based on distance from maxValue and the NPC's personality-set rewardWeight.
@@ -49,6 +69,7 @@ public class PersonalityController : MonoBehaviour
         randomizationFactor = 0.5f;
         moneyNeededPerDay = 60; // Based off eating 3X per day, sleeping 1X per day, and watching TV 2X per day.
         npcController = GetComponent<NPCController>();
+        GeneratePersonality(); // IF THERE IS NO PERSONALITY TAGS
     }
 
     void Update()
