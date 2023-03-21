3e6ce2f5657bbb557df92590531b66c86ecbc650
Collin Butterworth <cb264195@falmouth.ac.uk>
2023-03-21 16:15:59 +0000

    Added comments for SceneController

--- Unity/COMP250 AI Project/Assets/Scripts/SceneController.cs
+++ Unity/COMP250 AI Project/Assets/Scripts/SceneController.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Class <c>SceneController</c> is used for functions that interact with the game level and scene.
+/// </summary>
 public class SceneController : MonoBehaviour
 {
+    /// <summary>
+    /// Method <c>SceneReset</c> resets the scene.
+    /// </summary>
     public void SceneReset()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
