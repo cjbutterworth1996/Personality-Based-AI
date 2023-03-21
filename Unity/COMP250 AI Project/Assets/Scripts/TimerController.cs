086df353a3cf593de7d479a0b009080078830c61
Collin Butterworth <cb264195@falmouth.ac.uk>
2023-03-21 16:32:55 +0000

    Added comments to TimerController script

--- Unity/COMP250 AI Project/Assets/Scripts/TimerController.cs
+++ Unity/COMP250 AI Project/Assets/Scripts/TimerController.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Timers;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>TimerController</c> handles timers, mainly the timer for Needs decay.
+/// </summary>
 public class TimerController : MonoBehaviour
 {
-    public int timerSpeed;
-    private Timer timer;
-    private int timerLength;
-    private NPCController npc;
-    private UIController ui;
+    int timerLength; // Length of the timer.
+    int timerSpeed; // Speed of the timer in seconds.
+    Timer timer; // The timer for Needs decay.
 
+    NPCController npc; // Reference to the attached NPCController script.
+
+    /// <summary>
+    /// Method <c>Start</c> is called before the first frame.
+    /// </summary>
     void Start()
     {
-        timerSpeed = 1;
-        timerLength = 100;
+        timerSpeed = 1; //Timer moves by this value per second.
+        timerLength = 100; //The total timer value.
         npc = GetComponent<NPCController>();
 
         // This timer will repeat indefinitely, and it ticks every second.
@@ -27,7 +33,9 @@ public class TimerController : MonoBehaviour
         InvokeRepeating("UpdateBars", 1f, 1f);
     }
 
-    // Updates the NPC's Needs ProgressBars based on their decreaseSpeed.
+    /// <summary>
+    /// Method <c>UpdateBars</c> updates the UI progress bars in realtime.
+    /// </summary>
     void UpdateBars()
     {
         npc.bladder.UpdateBar(npc.bladder.bar, npc.bladder.currentValue -= npc.bladder.decreaseSpeed);
