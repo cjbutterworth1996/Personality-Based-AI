8f0d5c62bd790bc2ed536b1b2fb807cacf4383ea
Collin Butterworth <cb264195@falmouth.ac.uk>
2023-03-21 16:37:07 +0000

    Added comments to UIController script

--- Unity/COMP250 AI Project/Assets/Scripts/UIController.cs
+++ Unity/COMP250 AI Project/Assets/Scripts/UIController.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+/// <summary>
+/// Class <c>UIController</c> is used to handle all UI functionality.
+/// </summary>
 public class UIController : MonoBehaviour
 {
-    public ProgressBar bladderBar;
-    public ProgressBar boredomBar;
-    public ProgressBar energyBar;
-    public ProgressBar hungerBar;
-    public ProgressBar thirstBar;
-    public Label moneyCounter;
-    private int defaultValue;
-    public UIDocument uiDoc;
+    public ProgressBar bladderBar; // The bladder progress bar.
+    public ProgressBar boredomBar; // The boredom progress bar.
+    public ProgressBar energyBar; // The energy progress bar.
+    public ProgressBar hungerBar; // The hunger progress bar.
+    public ProgressBar thirstBar; // The thirst progress bar.
+    public Label moneyCounter; // The money counter.
+    public UIDocument uiDoc; // The UIDocument in which these progress bars are stored.
 
+    int defaultValue; // The default value of each bar.
+
+    /// <summary>
+    /// Method <c>Start</c> is called before the first frame.
+    /// </summary>
     void Start()
     {
-        this.enabled = true;
+        this.enabled = true; //Ensures this script is enabled.
         uiDoc = GetComponent<UIDocument>();
         defaultValue = 100;
         bladderBar = uiDoc.rootVisualElement.Q<ProgressBar>("BladderBar");
@@ -30,9 +37,14 @@ public class UIController : MonoBehaviour
         energyBar.value = defaultValue;
         hungerBar.value = defaultValue;
         thirstBar.value = defaultValue;
-        moneyCounter.text = "Money: " + defaultValue.ToString();
+
+        UpdateMoney(defaultValue);
     }
 
+    /// <summary>
+    /// Method <c>UpdateMoney</c> updates the money counter UI element.
+    /// </summary>
+    /// <param name="updateAmount"></param>
     public void UpdateMoney(int updateAmount)
     {
         moneyCounter.text = "Money: " + updateAmount.ToString();
