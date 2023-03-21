694656313a1d2b755c72f56d165d1c2b1ecc5271
Collin Butterworth <cb264195@falmouth.ac.uk>
2023-03-21 13:27:07 +0000

    Added a ProximityModifier script

---
+++ Unity/COMP250 AI Project/Assets/Scripts/ProximityModifier.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityModifier : MonoBehaviour
+{
+    public int bladderRateChange;
+    public int boredomRateChange;
+    public int energyRateChange;
+    public int hungerRateChange;
+    public int thirstRateChange;
+    public float bladderWeightChange;
+    public float boredomWeightChange;
+    public float energyWeightChange;
+    public float hungerWeightChange;
+    public float thirstWeightChange;
+}
